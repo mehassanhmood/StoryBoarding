b806c33670daa8a1177851938541672642a0f40e
mehassanhmood <mehassanhmood@gmail.com>
2024-09-30 12:57:32 -0400

    css refactored again

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
 import React from "react";
 import HomePage from "./components/HomePage";
 import Persona from "./components/Persona";
+
 function App() {
   return (
-    <div className="flex justify-center items-center h-screen bg-teal-300">
-      <HomePage /> 
+    <div className="flex flex-col justify-center items-center h-screen bg-teal-300 p-4">
+      <HomePage />
       <Persona />
-
     </div>
   );
 }
 
 export default App;
-
