26548bfa0a2560f1c99ab9016bee86252dc906ed
mehassanhmood <mehassanhmood@gmail.com>
2024-09-30 14:54:39 -0400

    Ap refactored

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,14 @@ import DollarWeakening from "./components/DollarWeakening";
 
 function App() {
   return (
-    <div className="flex flex-col justify-center items-center h-screen bg-teal-300 p-4">
+    <div className="flex flex-col justify-center items-center min-h-screen bg-teal-300 p-4">
       <HomePage />
-      <Persona />
-      <DollarWeakening />
+      <div className="mt-4 w-full max-w-3xl">
+        <Persona />
+      </div>
+      <div className="mt-4 w-full max-w-3xl">
+        <DollarWeakening />
+      </div>
     </div>
   );
 }
