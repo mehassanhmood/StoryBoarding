7059e0e083758f15d7844ece671bec823da33c06
mehassanhmood <mehassanhmood@gmail.com>
2024-09-30 17:35:53 -0400

    Final Touches

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const HomePage = () => {
   return (
     <div>
-      <h2>Inflation, Income, Inequality.</h2>
+      <h1 className='text-xxl'>Inflation, Income, Inequality.</h1>
       <p>The impact and gradual increase in the evaporating buying power of a dollar.</p>
       <p>How to navigate through this problem?</p>
     </div>
