ea7af1d7f0242fbf77ec136f8472e24fdae19a48
mehassanhmood <mehassanhmood@gmail.com>
2024-09-30 17:31:24 -0400

    Home page Fixed

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const HomePage = () => {
   return (
-    <div>Homepage</div>
+    <div>
+      <h2>Inflation, Income, Inequality.</h2>
+      <p>The impact and gradual increase in the evaporating buying power of a dollar.</p>
+      <p>How to navigate through this problem?</p>
+    </div>
   )
 }
 
