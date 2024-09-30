1fb51f24c3aefe493c93eb233951755c5c0210e6
mehassanhmood <mehassanhmood@gmail.com>
2024-09-30 12:27:08 -0400

    persona added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import HomePage from "./components/HomePage";
+import Persona from "./components/Persona";
 function App() {
   return (
     <div className="flex justify-center items-center h-screen bg-gray-100">
       <HomePage></HomePage>
+      <Persona />
 
     </div>
   );
