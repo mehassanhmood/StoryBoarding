72a64b6d8a996dc8ee0e4911d91d28d508a8df9e
mehassanhmood <mehassanhmood@gmail.com>
2024-09-30 15:44:11 -0400

    Adapted persona updated

--- src/components/AdaptedPersona.jsx
+++ src/components/AdaptedPersona.jsx
@@ -5,11 +5,11 @@ const AdaptedPersona = ({ name }) => {
     return (
       <div className="p-4 border rounded-lg shadow-md">
         <h2 className="text-xl font-bold">{name}</h2>
-        <p>"1 %ers"</p>
+        <p><b>Amjad</b></p>
         <img 
           src={APersona}
-          alt={`${name} Image`} // Alt text for accessibility
-          className="mt-2 w-full h-auto rounded" // Tailwind CSS classes for styling
+          alt={`${name} Image`} 
+          className="mt-2 w-full h-auto rounded" 
         />
       </div>
     );
