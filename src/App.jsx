e3bdc94ab46750649d6d2acc1b40421c1e69cac4
mehassanhmood <mehassanhmood@gmail.com>
2024-10-02 18:06:48 -0400

    components arrangement

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import AdaptedPersona from "./components/AdaptedPersona";
 function App() {
   return (
     
-      <div className="py-6 px-2 rounded-lg">
+      <div className="py-6 px-2 border rounded-lg">
       <HomePage />
       {/* <div className="mt-4 w-full max-w-3xl"> */}
       <div>
