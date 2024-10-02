78d067cd89e41dc17b2675f8ad5f6c031db2dc83
Hassan Mehmood <mehassanhmood@gmail.com>
2024-10-02 01:14:30 -0400

    font size

--- src/components/DollarWeakening.jsx
+++ src/components/DollarWeakening.jsx
@@ -8,10 +8,10 @@ const DollarWeakening = () => {
     <div className="p-4 border rounded-lg shadow-md bg-teal-500 flex">
       <div className="flex-1 text-left p-4">
         <h3 className="text-lg font-semibold">Impact of Dollar Weakening</h3>
-        <p>
+        <p className='text-xl'>
           And then, there was the glaring reality, the same dollar that bought her parents groceries in the 1990s now barely covered half of what it used to. It struck her that what used to be $1 back then now felt like $3 today.
         </p>
-        <p>The dollar had depreciated over time, leaving her and Steve with less buying power, even as prices for basic goods continued to climb.</p>
+        <p className='text-xl'>The dollar had depreciated over time, leaving her and Steve with less buying power, even as prices for basic goods continued to climb.</p>
       </div>
       <div className="flex-shrink-0 flex flex-col items-end ml-4">
         <img 
