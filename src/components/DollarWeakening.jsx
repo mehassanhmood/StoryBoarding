20e20697720574a3709c2aee2f938b559e512985
Hassan Mehmood <mehassanhmood@gmail.com>
2024-10-02 00:57:12 -0400

    more human touch

--- src/components/DollarWeakening.jsx
+++ src/components/DollarWeakening.jsx
@@ -5,30 +5,28 @@ import inflation from "../../public/inflation_pic.jpg";
 
 const DollarWeakening = () => {
   return (
-    <div className="p-4 border rounded-lg shadow-md bg-teal-500 flex items-center">
-      <div className="flex-shrink-0">
-        <img 
-          src={weakingingImage} 
-          className="max-w-full h-auto rounded-md mb-2" 
-          alt="Dollar Weakening"
-        />
-      </div>
-      <div className="flex-1 text-center p-4">
+    <div className="p-4 border rounded-lg shadow-md bg-teal-500 flex">
+      <div className="flex-1 text-left p-4">
         <h3 className="text-lg font-semibold">Impact of Dollar Weakening</h3>
         <p>
           And then, there was the glaring reality, the same dollar that bought her parents groceries in the 1990s now barely covered half of what it used to. It struck her that what used to be $1 back then now felt like $3 today.
         </p>
         <p>The dollar had depreciated over time, leaving her and Steve with less buying power, even as prices for basic goods continued to climb.</p>
       </div>
-      <div className="flex-shrink-0">
+      <div className="flex-shrink-0 flex flex-col items-end ml-4">
         <img 
-          src={ inflation} 
-          className="max-w-full h-auto rounded-md mb-2" 
-          alt="infaltion"
+          src={weakingingImage} 
+          className="max-w-xs h-auto rounded-md mb-2" 
+          alt="Dollar Weakening"
+        />
+        <img 
+          src={inflation} 
+          className="max-w-xs h-auto rounded-md mb-2" 
+          alt="Inflation"
         />
         <img 
           src={weakingingImage1990}
-          className="max-w-full h-auto rounded-md" 
+          className="max-w-xs h-auto rounded-md" 
           alt="Dollar Weakening in 1990"
         />
       </div>
@@ -37,6 +35,7 @@ const DollarWeakening = () => {
 };
 
 export default DollarWeakening;
+
 // const DollarWeakening = () => {
 //   return (
 //     <div className="p-4 border rounded-lg shadow-md bg-teal-500">
