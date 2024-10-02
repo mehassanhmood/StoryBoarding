27506493ef2aec9f7146590038b56a5200d30500
Hassan Mehmood <mehassanhmood@gmail.com>
2024-10-02 01:07:16 -0400

    font size

--- src/components/Persona.jsx
+++ src/components/Persona.jsx
@@ -36,7 +36,7 @@ const Persona = () => {
         <h2 className="text-xl font-bold">
           <b>Shakira:</b> and her family of 3:
         </h2>
-        <p>
+        <p className='text-xl'>
           In a small, sunlit apartment on the outskirts of a bustling city,
           Shakira sat at the kitchen table, her brow furrowed with worry. Bills
           were strewn across the table, reminders of the tightrope she and her
@@ -82,52 +82,3 @@ const Persona = () => {
 };
 
 export default Persona;
-
-// import React, { useEffect, useRef, useState } from 'react';
-// import personaImage from "../../public/persona.jpeg";
-// import inequalitImage from "../../public/income-inequality.png";
-
-// const Persona = () => {
-//   return (
-//     <div className="p-4 border rounded-lg shadow-md bg-teal-100">
-//       <div className="mb-4">
-//         <h2 className="text-xl font-bold">
-//           <b>Shakira:</b> and her family of 3:
-//         </h2>
-//         <p>
-//           In a small, sunlit apartment on the outskirts of a bustling city,
-//           Shakira sat at the kitchen table, her brow furrowed with worry. Bills
-//           were strewn across the table, reminders of the tightrope she and her
-//           husband, Steve, walked every month.
-//         </p>
-//         <p className="mt-2">Their goals:</p>
-//         <ul className="list-disc list-inside mb-2">
-//           <li>Home</li>
-//           <li>Food</li>
-//           <li>Education for their daughter, Lily</li>
-//         </ul>
-//         <p>The three basic needs of a human.</p>
-//         <img
-//           src={personaImage}
-//           alt="Shakira and her family"
-//           className="w-full h-auto rounded-lg mt-4 opacity-0 transition-opacity duration-500"
-//         />
-//       </div>
-//       <div className="placeholder-chart border-dashed border-2 border-gray-300 h-48 flex items-center justify-center mb-4">
-//         <span className="text-gray-500 p-2">
-//           In the back of her mind, Shakira couldn't shake the image of the
-//           curve she’d seen in the news recently—one showing how income
-//           inequality had widened over the years. The top 1% was growing
-//           wealthier, while families like hers were left struggling.
-//         </span>
-//         <img
-//           className="max-w-full h-full object-cover rotate-left"
-//           src={inequalitImage}
-//           alt="Income Inequality"
-//         />
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Persona;
--- src/components/SPReturns.jsx
+++ src/components/SPReturns.jsx
@@ -34,34 +34,3 @@ const SPReturns = () => {
 };
 
 export default SPReturns;
-
-// SPReturns.js
-// import React from 'react';
-// import SPY from "../../public/SPY.png";
-
-// const SPReturns = () => {
-
-
-//   return (
-//     <div className="p-4 border rounded-lg shadow-md">
-      
-//       <div className="mb-4">
-//         <p> <b> Beating the odds:</b></p> <br />
-//         <p>
-//         As Shakira sat at the table, her mind drifted to an article from <b>Morningstar: "The Rise of Passive Investing"</b> she had recently read. It wasn’t just experts and wealthy investors who had found ways to shield themselves from inflation—40% of investors were now hedging inflation through passive indexing
-//         </p> <br />
-//         <p>
-//         What truly surprised her was the fact that many who didn’t succeed lacked basic financial literacy. Studies showed that millennials adopting passive investing were far better prepared for retirement compared to those who had no investments, highlighting how essential understanding these strategies had become, according to <strong>"Millennials and Retirement: How to Prepare." Bankrate, 2022. </strong>
-//         </p>
-//       </div>
-//       <img
-//           className="max-w-full h-full object-cover rotate-left"
-//           src={SPY}
-//           alt="Income Inequality"
-//         />
-//     </div>
-//   );
-// };
-
-
-// export default SPReturns;
