bc8dd5164e9194c51ba90010c4d3764b123cafc9
Hassan Mehmood <mehassanhmood@gmail.com>
2024-10-03 01:28:11 -0400

    responsiveness

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const HomePage = () => {
   return (
-    <section className="flex flex-col justify-center items-center bg-indigo-200 shadow-lg shadow-indigo-500 hover:shadow-indigo-900 p-6 md:p-12 border rounded-lg my-4 mx-2 md:mx-8">
+    <section className="flex flex-col justify-center items-center bg-indigo-200 shadow-lg hover:shadow-indigo-900 p-6 md:p-12 border rounded-lg my-4 mx-2 md:mx-8">
       <div className="max-w-lg md:max-w-2xl text-center">
         <h1 className="text-2xl md:text-4xl font-bold text-gray-900 mb-6">
           Inflation, Income, Inequality
